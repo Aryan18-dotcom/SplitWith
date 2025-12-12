6b396fd974bc12586205ec1c763e448d63bc7925
Aryan Chheda <aryanchheda18@gmail.com>
2025-12-12 17:59:26 +0530

    Make the App Responsive

--- wsgi.py
+++ wsgi.py
@@ -3,7 +3,4 @@ from app import create_app
 app = create_app()
 
 if __name__ == "__main__":
-    # Bind to 0.0.0.0 and use environment PORT
-    import os
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(debug=True)
